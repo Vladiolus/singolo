325da42a3dbd9fc9b67c7b074a87fb54fdaf0396
Vladiolus <uda4a.vlad@mail.ru>
2020-03-17 19:31:57 +0300

    feat: interactive header

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // script for Singolo
+let selectedNav;
+
+nav.onclick = function(event) {
+  let a = event.target.closest("a");
+  if (!a) return;
+  if (!nav.contains(a)) return;
+  
+  activate(a);
+}
+
+function activate(a) {
+  if (selectedNav) {
+    selectedNav.classList.remove("active");
+  }
+  selectedNav = a;
+  selectedNav.classList.add("active");
+}
