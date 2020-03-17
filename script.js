beae380fbe0652c921195ad0f8168c59a39a58b6
Vladiolus <uda4a.vlad@mail.ru>
2020-03-18 01:28:35 +0300

    feat: improve header

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // script for Singolo
+let html = document.documentElement;
 let selectedNav;
+let posServices = document.getElementById("services").offsetTop - 96;
+let posPortfolio = document.getElementById("portfolio").offsetTop - 96;
+let posAbout = document.getElementById("about").offsetTop - 96;
+let posContact = document.getElementById("contact").offsetTop - 96;
+let posBottom = document.body.scrollHeight;
 
-nav.onclick = function(event) {
+nav.addEventListener("click", function(event) {
   let a = event.target.closest("a");
   if (!a) return;
   if (!nav.contains(a)) return;
   
   activate(a);
-}
+});
 
 function activate(a) {
   if (selectedNav) {
@@ -16,3 +22,11 @@ function activate(a) {
   selectedNav = a;
   selectedNav.classList.add("active");
 }
+
+window.addEventListener("scroll", function() {
+  if (html.scrollTop < posServices) activate(document.querySelectorAll("nav a")[0]);
+  if (posServices < html.scrollTop && html.scrollTop < posPortfolio) activate(document.querySelectorAll("nav a")[1]);
+  if (posPortfolio < html.scrollTop && html.scrollTop < posAbout) activate(document.querySelectorAll("nav a")[2]);
+  if (posAbout < html.scrollTop && html.scrollTop < posContact) activate(document.querySelectorAll("nav a")[3]);
+  if (posContact < html.scrollTop || html.scrollTop+html.clientHeight == posBottom) activate(document.querySelectorAll("nav a")[4]);
+});
