b73c1c9d397a4f28039bed132a1ef96fe1439501
Vladiolus <uda4a.vlad@mail.ru>
2020-03-18 13:02:42 +0300

    fix: optimize header

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 // script for Singolo
-let html = document.documentElement;
+const HTML = document.documentElement;
 let selectedNav;
-let posServices = document.getElementById("services").offsetTop - 96;
-let posPortfolio = document.getElementById("portfolio").offsetTop - 96;
-let posAbout = document.getElementById("about").offsetTop - 96;
-let posContact = document.getElementById("contact").offsetTop - 96;
-let posBottom = document.body.scrollHeight;
 
-nav.addEventListener("click", function(event) {
-  let a = event.target.closest("a");
-  if (!a) return;
-  if (!nav.contains(a)) return;
-  
-  activate(a);
-});
+// Header //
 
 function activate(a) {
   if (selectedNav) {
@@ -23,11 +12,43 @@ function activate(a) {
   selectedNav.classList.add("active");
 }
 
-//window.addEventListener("scroll", function() {
+// There is no need in activation on click - scroll will activate menu automatically.
+/*
+document.getElementById("nav").addEventListener("click", function(event) {
+  let a = event.target.closest("a");
+  if (!a) return;
+  if (!nav.contains(a)) return;
+  
+  activate(a);
+});
+*/
+
+window.addEventListener("scroll", function() {
+  let elList = document.querySelectorAll("section");
+  let index;
+  for (let i = 0; i < elList.length; i++) {
+    if (elList[i].offsetTop - 96 < HTML.scrollTop && HTML.scrollTop < elList[i].offsetTop - 96 + elList[i].offsetHeight) {
+      index = i;
+    }
+  }
+  if (HTML.scrollTop + HTML.clientHeight == HTML.scrollHeight) index = elList.length - 1;
+  activate(document.querySelectorAll("#nav a")[index]);
+});
+
+// Old variant of scroll. A little bit too specific.
+
+/*
+let posServices = document.getElementById("services").offsetTop - 96;
+let posPortfolio = document.getElementById("portfolio").offsetTop - 96;
+let posAbout = document.getElementById("about").offsetTop - 96;
+let posContact = document.getElementById("contact").offsetTop - 96;
+let posBottom = HTML.scrollHeight;
+
 setInterval(function() {
-  if (html.scrollTop < posServices) activate(document.querySelectorAll("nav a")[0]);
-  if (posServices < html.scrollTop && html.scrollTop < posPortfolio) activate(document.querySelectorAll("nav a")[1]);
-  if (posPortfolio < html.scrollTop && html.scrollTop < posAbout) activate(document.querySelectorAll("nav a")[2]);
-  if (posAbout < html.scrollTop && html.scrollTop < posContact) activate(document.querySelectorAll("nav a")[3]);
-  if (posContact < html.scrollTop || html.scrollTop+html.clientHeight == posBottom) activate(document.querySelectorAll("nav a")[4]);
+  if (HTML.scrollTop  < posServices)                                                      activate(document.querySelectorAll("#nav a")[0]);
+  if (posServices     < HTML.scrollTop && HTML.scrollTop < posPortfolio)                  activate(document.querySelectorAll("#nav a")[1]);
+  if (posPortfolio    < HTML.scrollTop && HTML.scrollTop < posAbout)                      activate(document.querySelectorAll("#nav a")[2]);
+  if (posAbout        < HTML.scrollTop && HTML.scrollTop < posContact)                    activate(document.querySelectorAll("#nav a")[3]);
+  if (posContact      < HTML.scrollTop || HTML.scrollTop+HTML.clientHeight == posBottom)  activate(document.querySelectorAll("#nav a")[4]);
 }, 1);
+*/
